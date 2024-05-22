f8873a04180ef4f43facb74c1609947f3b8468e6
Gerysnz <gerardfuentesaf@gmail.com>
2024-05-22 19:45:28 +0200

    version9
    en el main ya se pueden crear tropas y el printstat se actualiza correctmanete (falta automatizar los recursos)

--- M3/src/pw/main.java
+++ M3/src/pw/main.java
@@ -3,12 +3,25 @@ package pw;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws resourceException{
 		planet planet1 = new planet();
+		planet1.setMetal(200000);
+        planet1.setDeuterium(54500);
+        planet1.setTechnologyDefense(3);
+        planet1.setTechnologyAtack(2);
+        planet1.newArmoredShip(1);
+        planet1.newBattleShip(1);
+        planet1.newLigthHunter(3);
+        planet1.newMissileLauncher(1);
+        planet1.newIonCannon(1);
+        planet1.newPlasmaCannon(1);
+        planet1.newHeavyHunter(1);     
+        
 		Comprobar comprobacion = new Comprobar();
 		final String menu00 = "\n1)View Planet Stats\n2)Build\n3)Upgrade Technology\n4)View Battle Reports\n0)Exit";
 	    final String menu02 = "1)Build troops\n2)Build Defenses\n3)Go Back";
 	    final String menu021 = "1)Build LigthHunter\n2)Build HeavyHunter\n3)Build BattleShip\n4)Build ArmoredShip\n5)Go Back";
+	    final String menu022 = "1)Build Missile Launcher\n2)Build Ion Cannon\n3)Build Plasma Cannon\n4)Go Back";
 	    final String menu03 = "1)Upgrade Defense Technology.\n2)Upgrade Attack Technology.\n3)Go Back";
 	    final String menu05 = "Planet Wars\n1)View Planet Stats\n2)Build\n3)Upgrade Technology\n4)View Battle Reports\n5)View Thread Comming\n0)Exit";
 
@@ -35,16 +48,28 @@ public class main {
                              option = comprobacion.comprobarNumero();
                              switch (option){
                              case 1:
-                            	 System.out.println("LIGHTHUNTER");
+                            	 System.out.println("Amount of Units ");
+                            	 System.out.println("Amount: > ");
+                            	 int cantidad = comprobacion.comprobarNumero();
+                            	 planet1.newLigthHunter(cantidad);
                             	 break;
                              case 2:
-                            	 System.out.println("HEAVYHUNTER");
+                            	 System.out.println("Amount of Units ");
+                            	 System.out.println("Amount: > ");
+                            	 cantidad = comprobacion.comprobarNumero();
+                            	 planet1.newHeavyHunter(cantidad);
                             	 break;
                              case 3:
-                            	 System.out.println("BATTLESHIP");
+                            	 System.out.println("Amount of Units ");
+                            	 System.out.println("Amount: > ");
+                            	 cantidad = comprobacion.comprobarNumero();
+                            	 planet1.newBattleShip(cantidad);
                             	 break;
                              case 4:
-                            	 System.out.println("ARMOREDSHIP");
+                            	 System.out.println("Amount of Units ");
+                            	 System.out.println("Amount: > ");
+                            	 cantidad = comprobacion.comprobarNumero();
+                            	 planet1.newArmoredShip(cantidad);
                             	 break;
                              case 5:
                             	 salir = true;
@@ -57,8 +82,33 @@ public class main {
                 	 salir = false;
                      break;     	 
                      case 2:
-                    	 System.out.println("OPCION 2");
-                    	 break;
+                    	 salir = false;
+                    	 while (!salir) {
+                    		 System.out.println(menu022);
+                             System.out.println("Option > ");
+                             option = comprobacion.comprobarNumero();
+                             switch (option){
+                             case 1:
+                            	 System.out.println("MISSILE LAUNCHER");
+                            	 break;
+                             case 2:
+                            	 System.out.println("ION CANNON");
+                            	 break;
+                             case 3:
+                            	 System.out.println("PLASMA CANNON");
+                            	 break;
+                            
+                             case 4:
+                            	 salir = true;
+                            	 break;
+                             default:
+          						System.out.println("\nInvalid option\n");
+                            
+                             }          
+                            	}
+                     salir = false;
+                     break;
+                    	
                      case 3:
                     	 salir = true;
                     	 break;
@@ -96,7 +146,7 @@ public class main {
             	System.out.println("Aqui va el printStat");
             	break;
             
-            case 0: // Cierra el programa
+            case 0:
                 System.out.println("Exiting...");
                 salir = true;
                 break;
--- M3/src/pw/planet.java
+++ M3/src/pw/planet.java
@@ -113,138 +113,191 @@ public class planet implements variables{
         }
     }
 
-    public void newLigthHunter(int n) throws resourceException{
+    public void newLigthHunter(int n) {
         int unidad = n;
+        int addedUnits = 0;
         for (int i = 0; i < n; i++) {
-            if (deuterium >= DEUTERIUM_COST_LIGTHHUNTER & metal >= METAL_COST_LIGTHHUNTER) {
-                int armor = ARMOR_LIGTHHUNTER + ((technologyDefense * PLUS_ARMOR_LIGTHHUNTER_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_LIGTHHUNTER + ((technologyAtack * PLUS_ATTACK_LIGTHHUNTER_BY_TECHNOLOGY) * BASE_DAMAGE_LIGTHHUNTER / 100);
-                deuterium -= DEUTERIUM_COST_LIGTHHUNTER;
-                metal -= METAL_COST_LIGTHHUNTER;
-                army[0].add(new ligthHunter());
-            } else {
-                throw new resourceException("Deuterium insuficiente para construir unidades.");
+            try {
+                if (deuterium >= DEUTERIUM_COST_LIGTHHUNTER & metal >= METAL_COST_LIGTHHUNTER) {
+                    int armor = ARMOR_LIGTHHUNTER + ((technologyDefense * PLUS_ARMOR_LIGTHHUNTER_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_LIGTHHUNTER + ((technologyAtack * PLUS_ATTACK_LIGTHHUNTER_BY_TECHNOLOGY) * BASE_DAMAGE_LIGTHHUNTER / 100);
+                    deuterium -= DEUTERIUM_COST_LIGTHHUNTER;
+                    metal -= METAL_COST_LIGTHHUNTER;
+                    army[0].add(new ligthHunter());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a LightHunter.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage()); 
+                break; 
             }
         }
-    }
-    public void newHeavyHunter(int n) throws resourceException {
-    	int unidad = n;
-        for (int i = 0; i < n; i++) {
-            if (deuterium >= DEUTERIUM_COST_HEAVYHUNTER & metal >= METAL_COST_HEAVYHUNTER) {
-                int armor = ARMOR_HEAVYHUNTER + ((technologyDefense * PLUS_ARMOR_HEAVYHUNTER_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_HEAVYHUNTER + ((technologyAtack * PLUS_ATTACK_HEAVYHUNTER_BY_TECHNOLOGY) * BASE_DAMAGE_HEAVYHUNTER / 100);
-                deuterium -= DEUTERIUM_COST_HEAVYHUNTER;
-                metal -= METAL_COST_HEAVYHUNTER;
-                army[1].add(new heavyHunter());
-            } else {
-                throw new resourceException("Deuterium insuficiente para construir unidades.");
-            }
-            }
-
+        System.out.println("Added "+ addedUnits +" LigthHunters");
     }
 
-    public void newBattleShip(int n) throws resourceException {
-    	int unidad = n;
+    public void newHeavyHunter(int n) {
+        int unidad = n;
+        int addedUnits = 0;
         for (int i = 0; i < n; i++) {
-            if (deuterium >= DEUTERIUM_COST_BATTLESHIP & metal >= METAL_COST_BATTLESHIP) {
-                int armor = ARMOR_BATTLESHIP + ((technologyDefense * PLUS_ARMOR_BATTLESHIP_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_BATTLESHIP + ((technologyAtack * PLUS_ATTACK_BATTLESHIP_BY_TECHNOLOGY) * BASE_DAMAGE_BATTLESHIP / 100);
-                deuterium -= DEUTERIUM_COST_BATTLESHIP;
-                metal -= METAL_COST_BATTLESHIP;
-                army[2].add(new battleShip());
-            } else {
-                throw new resourceException("Deuterium insuficiente para construir unidades.");
+            try {
+                if (deuterium >= DEUTERIUM_COST_HEAVYHUNTER & metal >= METAL_COST_HEAVYHUNTER) {
+                    int armor = ARMOR_HEAVYHUNTER + ((technologyDefense * PLUS_ARMOR_HEAVYHUNTER_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_HEAVYHUNTER + ((technologyAtack * PLUS_ATTACK_HEAVYHUNTER_BY_TECHNOLOGY) * BASE_DAMAGE_HEAVYHUNTER / 100);
+                    deuterium -= DEUTERIUM_COST_HEAVYHUNTER;
+                    metal -= METAL_COST_HEAVYHUNTER;
+                    army[1].add(new heavyHunter());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a HeavyHunter.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage()); 
+                break; 
             }
-            }
-            }
-
-    public void newArmoredShip(int n) throws resourceException {
-    	int unidad = n;
-        for (int i = 0; i < n; i++) {
-            if (deuterium >= DEUTERIUM_COST_ARMOREDSHIP & metal >= METAL_COST_ARMOREDSHIP) {
-                int armor = ARMOR_ARMOREDSHIP + ((technologyDefense * PLUS_ARMOR_ARMOREDSHIP_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_ARMOREDSHIP + ((technologyAtack * PLUS_ATTACK_ARMOREDSHIP_BY_TECHNOLOGY) * BASE_DAMAGE_ARMOREDSHIP / 100);
-                deuterium -= DEUTERIUM_COST_ARMOREDSHIP;
-                metal -= METAL_COST_ARMOREDSHIP;
-                army[3].add(new heavyHunter());
-            } else {
-                throw new resourceException("Deuterium insuficiente para construir unidades.");
-            }
-            }
-            }
-
-    public void newMissileLauncher(int n) throws resourceException{
-    	int unidad = n;
-        if (deuterium >= DEUTERIUM_COST_MISSILELAUNCHER & metal >= METAL_COST_MISSILELAUNCHER) {
-            for (int i = 0; i < unidad; i++) {
-                int armor = ARMOR_MISSILELAUNCHER + ((technologyDefense * PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_MISSILELAUNCHER + ((technologyAtack * PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY) * BASE_DAMAGE_MISSILELAUNCHER / 100);
-                deuterium -= DEUTERIUM_COST_MISSILELAUNCHER;
-                metal -= METAL_COST_MISSILELAUNCHER;
-                army[4].add(new missileLauncher());
-            }
-        } else {
-            throw new resourceException("Deuterium insuficiente para construir unidades.");
         }
-
+        System.out.println("Added "+ addedUnits +" HeavyHunters");
     }
 
-    public void newIonCannon(int n) throws resourceException{
-    	int unidad = n;
-        if (deuterium >= DEUTERIUM_COST_IONCANNON & metal >= METAL_COST_IONCANNON) {
-            for (int i = 0; i < unidad; i++) {
-                int armor = ARMOR_IONCANNON + ((technologyDefense * PLUS_ARMOR_IONCANNON_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_IONCANNON + ((technologyAtack * PLUS_ATTACK_IONCANNON_BY_TECHNOLOGY) * BASE_DAMAGE_IONCANNON / 100);
-                deuterium -= DEUTERIUM_COST_IONCANNON;
-                metal -= METAL_COST_IONCANNON;
-                army[5].add(new ionCannon());
+    public void newBattleShip(int n) {
+        int unidad = n;
+        int addedUnits = 0;
+        for (int i = 0; i < n; i++) {
+            try {
+                if (deuterium >= DEUTERIUM_COST_BATTLESHIP & metal >= METAL_COST_BATTLESHIP) {
+                    int armor = ARMOR_BATTLESHIP + ((technologyDefense * PLUS_ARMOR_BATTLESHIP_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_BATTLESHIP + ((technologyAtack * PLUS_ATTACK_BATTLESHIP_BY_TECHNOLOGY) * BASE_DAMAGE_BATTLESHIP / 100);
+                    deuterium -= DEUTERIUM_COST_BATTLESHIP;
+                    metal -= METAL_COST_BATTLESHIP;
+                    army[2].add(new battleShip());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a BattleShips.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage()); 
+                break; 
             }
-        } else {
-            throw new resourceException("Deuterium insuficiente para construir unidades.");
         }
+        System.out.println("Added "+ addedUnits +" BattleShips");
+    }
 
+    public void newArmoredShip(int n) {
+        int unidad = n;
+        int addedUnits = 0;
+        for (int i = 0; i < n; i++) {
+            try {
+                if (deuterium >= DEUTERIUM_COST_ARMOREDSHIP & metal >= METAL_COST_ARMOREDSHIP) {
+                    int armor = ARMOR_ARMOREDSHIP + ((technologyDefense * PLUS_ARMOR_ARMOREDSHIP_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_ARMOREDSHIP + ((technologyAtack * PLUS_ATTACK_ARMOREDSHIP_BY_TECHNOLOGY) * BASE_DAMAGE_ARMOREDSHIP / 100);
+                    deuterium -= DEUTERIUM_COST_ARMOREDSHIP;
+                    metal -= METAL_COST_ARMOREDSHIP;
+                    army[3].add(new armoredShip());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a ArmoredShips.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage()); 
+                break; 
+            }
+        }
+        System.out.println("Added "+ addedUnits +" ArmoredShips");
+    }
+
+    public void newMissileLauncher(int n) {
+        int addedUnits = 0;
+        for (int i = 0; i < n; i++) {
+            try {
+                if (deuterium >= DEUTERIUM_COST_MISSILELAUNCHER & metal >= METAL_COST_MISSILELAUNCHER) {
+                    int armor = ARMOR_MISSILELAUNCHER + ((technologyDefense * PLUS_ARMOR_MISSILELAUNCHER_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_MISSILELAUNCHER + ((technologyAtack * PLUS_ATTACK_MISSILELAUNCHER_BY_TECHNOLOGY) * BASE_DAMAGE_MISSILELAUNCHER / 100);
+                    deuterium -= DEUTERIUM_COST_MISSILELAUNCHER;
+                    metal -= METAL_COST_MISSILELAUNCHER;
+                    army[4].add(new missileLauncher());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a MissileLauncher.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        System.out.println("Added " + addedUnits + " MissileLaunchers");
+    }
+
+
+    public void newIonCannon(int n) {
+        int addedUnits = 0;
+        for (int i = 0; i < n; i++) {
+            try {
+                if (deuterium >= DEUTERIUM_COST_IONCANNON & metal >= METAL_COST_IONCANNON) {
+                    int armor = ARMOR_IONCANNON + ((technologyDefense * PLUS_ARMOR_IONCANNON_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_IONCANNON + ((technologyAtack * PLUS_ATTACK_IONCANNON_BY_TECHNOLOGY) * BASE_DAMAGE_IONCANNON / 100);
+                    deuterium -= DEUTERIUM_COST_IONCANNON;
+                    metal -= METAL_COST_IONCANNON;
+                    army[5].add(new ionCannon());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build an IonCannon.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        System.out.println("Added " + addedUnits + " IonCannons");
     }
 
     
 
-    public void newPlasmaCannon(int n) throws resourceException{
-    	int unidad = n;
-        if (deuterium >= DEUTERIUM_COST_PLASMACANNON & metal >= METAL_COST_PLASMACANNON) {
-            for (int i = 0; i < unidad; i++) {
-                int armor = ARMOR_PLASMACANNON + ((technologyDefense * PLUS_ARMOR_PLASMACANNON_BY_TECHNOLOGY) * 1000 / 100);
-                int baseDamage = BASE_DAMAGE_PLASMACANNON + ((technologyAtack * PLUS_ATTACK_PLASMACANNON_BY_TECHNOLOGY) * BASE_DAMAGE_PLASMACANNON / 100);
-                deuterium -= DEUTERIUM_COST_PLASMACANNON;
-                metal -= METAL_COST_PLASMACANNON;
-                army[6].add(new plasmaCannon());
-
+    public void newPlasmaCannon(int n) {
+        int addedUnits = 0;
+        for (int i = 0; i < n; i++) {
+            try {
+                if (deuterium >= DEUTERIUM_COST_PLASMACANNON && metal >= METAL_COST_PLASMACANNON) {
+                    int armor = ARMOR_PLASMACANNON + ((technologyDefense * PLUS_ARMOR_PLASMACANNON_BY_TECHNOLOGY) * 1000 / 100);
+                    int baseDamage = BASE_DAMAGE_PLASMACANNON + ((technologyAtack * PLUS_ATTACK_PLASMACANNON_BY_TECHNOLOGY) * BASE_DAMAGE_PLASMACANNON / 100);
+                    deuterium -= DEUTERIUM_COST_PLASMACANNON;
+                    metal -= METAL_COST_PLASMACANNON;
+                    army[6].add(new plasmaCannon());
+                    addedUnits++;
+                } else {
+                    throw new resourceException("There's not enough resources to build a PlasmaCannon.");
+                }
+            } catch (resourceException e) {
+                System.out.println(e.getMessage());
+                break;
             }
-        } else {
-            throw new resourceException("Deuterium insuficiente para construir unidades.");
-            }
-    	
-
+        }
+        System.out.println("Added " + addedUnits + " PlasmaCannons");
     }
 
+    	
+
+    
+
     public void printStats() {
-            String datos = String.format("Planet Stats:\n\n" +
-                    "TECHNOLOGY\n\n" +
-                    "Atack Technology:       %d\n" +
-                    "Defense Technology:     %d\n\n" +
-                    "DEFENSES\n\n" +
-                    "Missile Launcher:       \n" +
-                    "Ion Cannon:             \n" +
-                    "Plasma Cannon:          \n\n" +
-                    "FLEET\n\n" +
-                    "Light Hunter:           \n" +
-                    "Heavy Hunter:           \n" +
-                    "Battle Ship:            \n" +
-                    "Armored Ship:           \n\n" +
-                    "RESOURCES\n\n" +
-                    "Metal:                  %d\n" +
-                    "Deuterium:              %d\n",
-                    technologyAtack, technologyDefense, metal, deuterium);
-            System.out.println(datos);
-        }
+    	String datos = String.format("Planet Stats:\n\n" +
+                "TECHNOLOGY\n\n" +
+                "Atack Technology:       %d\n" +
+                "Defense Technology:     %d\n\n" +
+                "DEFENSES\n\n" +
+                "Missile Launcher:       %d\n" +
+                "Ion Cannon:             %d\n" +
+                "Plasma Cannon:          %d\n\n" +
+                "FLEET\n\n" +
+                "Light Hunter:           %d\n" +
+                "Heavy Hunter:           %d\n" +
+                "Battle Ship:            %d\n" +
+                "Armored Ship:           %d\n\n" +
+                "RESOURCES\n\n" +
+                "Metal:          %d\n" +
+                "Deuterium:      %d\n",
+                technologyAtack, technologyDefense,army[4].size(),army[5].size(),army[6].size(),army[0].size(),army[1].size(),army[2].size(),army[3].size(), metal, deuterium);
+        System.out.println(datos);
+    }
 
     }
 
